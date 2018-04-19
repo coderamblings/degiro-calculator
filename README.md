1bafcade2d30a181acdddac007b699db1c328919
jj-owner <jj-owner@domain.com>
2018-04-20 00:29:32 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# degiro-calculator
+degiro-caculator README.md
+
+# Kalkulator zysków / strat Degiro
+TBD: 
+- manual in English
+- further automation of preparatory steps
+- rewrite for python3
+
+
+Programik napisany, by móc wygodniej wypełniać PIT, tudzież przejdzeć historię dokonań.
+
+Skrócony opis wykorzystania programu:
+
+1. Przygotuj bazę danych + użytkownika.
+2. Załaduj raport transakcji degiro do źródłowej tabeli, wygeneruj tabele dla poszczególnych firm.
+3. Odpal program.
+4. Analizuj.
+
+## Przygotuj bazę danych + użytkownika.
+
+```
+create database stockprofit;
+create user 'stockprofit'@'localhost';
+grant all privileges on stockprofit.* to 'stockprofit'@'localhost';
+
+flush privileges;
+```
+
+Wygeneruj raport transakcji w Degiro (od początku przygody z brokerem), w formacie CSV.
+Wrzuć plik z transakcjami (**Transactions.csv**) do katalogu bazy stockprofit (/var/lib/mysql/stockprofit/).
+
+## Załaduj raport transakcji degiro do źródłowej tabeli, wygeneruj tabele dla poszczególnych firm.
+
+Domyślnie Degiro generuje raport posortowany po dacie od operacji najnowszych, do najstarszych, potrzebujemy odwrócić tę kolejność, albo w arkuszu kalkulacyjnym, albo BASHem:
+```
+tac Transactions.csv > trx_tmp.csv
+mv -f trx_tmp.csv Transactions.csv
+```
+
+Odpal sesję w bazie użytkowikiem stockprofit, stwórz tabelę źródłową, załaduj to niej raport Degiro:
+```
+mysql stockprofit -ustockprofit
+create table input(date DATE, time date, stock_name varchar(150), ISIN varchar(20), stock_market varchar(7), stock_no int, currency1 varchar(3), stock_rate float, currency2 varchar(3), trx_local_value float, currency3 varchar(3), trx_value_pln float, exchange_rate float, currency4 varchar(3), degiro_fee float, currency5 varchar(3), total_cost float);
+
+create table sales (date date, stock_name varchar(150), sale_price_pln float, sale_value_pln float, sold_stock_no int, purchase_value_pln float, profit float);
+
+load data infile '/var/lib/mysql/stockprofit/Transactions.csv' into table input 
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES
+(@c1, time, stock_name, ISIN, stock_market, stock_no, currency1, stock_rate, currency2, trx_local_value, currency3, trx_value_pln, exchange_rate, currency4, degiro_fee, currency5, total_cost) 
+SET date = str_to_date(@c1, '%d-%m-%Y')
+;
+```
+
+Teraz trzeba sprawdzić poprawność wgranych danych - najczęstszy błąd: przecinek w nazwie akcji, który rozbija ilość kolumn w danym wierszu.
+```
+select * from input where stock_no = 0;
+select * from input where LENGTH(ISIN) <> 12;
+````
+Jeśli którakolwiek z powyższych kwerend zwraca wynik inny niż 0, dane wejściowe prawdopodobnie wymagają poprawki. Usuń nadmiarowe przecinki lub inne niespodzianki, przeczyść tabelę (`delete from input;`) i załaduj dane ponownie (`load data infile ...`).
+
+Skoro plik Transactions.csv został przeczyszczony, wracamy do BASHa i generujemy komendy do stworzenia tabel per firmy, przy okazji usuwając z nich cudzysłowy
+```
+awk -F ',' ' {print $3}' Transactions.csv | sort | uniq > table_list.txt
+sed -i 's/\"//g' table_list.txt
+sed -i "s/'//g" table_list.txt
+
+IFS=$'\n'
+for name in `cat table_list.txt`; do echo "create table \`$name\` (ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, date date, stock_name varchar(150), trx_value_pln float, available_stock_no int, orig_stock_no int, purchase_price_pln float);" >> table_list.sql; done;
+mysql -ustockprofit stockprofit < table_list.sql
+```
+Przy okazji, warto w tym miejscu przygotować sobie komendy to czyszczenia tych wszystkich tabel, gdyby skrypt po drodze się gdzieś wysypał, albo gdy zaktualizujemy dane źródłowe. Wykorzystujemy stworzony przed chwilą plik `table_list.txt`.
+```
+for name in `cat table_list.txt`; do echo "delete from  \`$name\`;" >> clear_tables.sql; done;
+echo "delete from sales;" >> clear_tables.sql
+```
+Jeśli trzeba będzie przeczyścić tabele, `mysql -ustockprofit stockprofit < clear_tables.sql`.
+
+Zweryfikuj poprawne stworzenie tabel  przy pomocy `show tables` odpalonym w bazie stockprofit - powinieneś zobaczyć tyle i takie tabele, ile i jakimi akcjami obracałeś.
+
+## Odpal program
+Jedyna zależność skryptu to pymysql, `pip install pymysql`, globalnie lub w śrdowisku virtualnym powinno załatwić sprawę.
+`python stock_profit.py`
+
+## Analizuj
+Np.
+```
+select sum(profit) from sales where YEAR(date) = 2017;
+select YEAR(date), sum(profit) from sales where stock_name = 'CI GAMES SA' group by YEAR(date);
+```
+
---
+++ stock_profit.py
@@ -0,0 +1,102 @@
+#!/bin/python
+
+# The idea of this app is to take a yearly transaction report from Degiro
+# and process the transactions in order to calculate the profit / loss
+# for taxation purposes
+
+import pymysql as m
+import pymysql.cursors
+
+def process_purchase(row):
+    """Gets a row read from inut table, picks selected columns and inserts into proper table"""
+    """Removes double and single quotes from stock names"""
+    original_name = row["stock_name"]
+    normalized_name1 = original_name.replace('"','')
+    normalized_name2 = normalized_name1.replace("'","")
+    #Table name which contains spaces needs to be surrounded by ticks, thus u0060
+    table_name = u"\u0060" + normalized_name2 + u"\u0060"
+    print 'Performing an insert of a purchase operation into' + table_name
+    #Row value which contains spaces needs to be surrounded by quotes, thus u0022
+    stock_name = u"\u0022" + normalized_name2 + u"\u0022"
+    sql = "INSERT INTO %s (date, stock_name, trx_value_pln, available_stock_no, orig_stock_no, purchase_price_pln) VALUES (\"%s\",%s,%s,%s,%s,%s)"
+    k.execute(sql % (table_name,row["date"],stock_name,row["trx_value_pln"],row["stock_no"],row["stock_no"],abs(row["trx_value_pln"]/row["stock_no"])))
+    return None
+
+def update_stock_inventory(row, table, stock_left):
+    """Invoked to subtract sold stock from given stock inventory table"""
+    sql = "UPDATE %s SET available_stock_no = %i where ID = %i"
+    u = connection.cursor()
+    u.execute(sql % (table, stock_left,row["ID"]))
+    u.connection.commit()
+    u.close()
+    return None
+
+def update_sales_table(row, stock_name, sale_value, purchase_cost):
+    #comment = "Sold %s of stock for %s, while the purchase cost was %s" % (abs(row["stock_no"]), sale_value, purchase_cost)
+    sql_sale_result = "INSERT INTO sales VALUES(\"%s\",\"%s\",%s,%s,%i,%s,%s)"
+    i = connection.cursor()
+    i.execute(sql_sale_result % (row["date"], stock_name, abs(sale_value / row["stock_no"]), sale_value, abs(row["stock_no"]), purchase_cost, sale_value - abs(purchase_cost)))
+    i.connection.commit()
+    i.close()
+    return None
+
+def process_sale(row):
+    """The heart of this app"""
+    stock_to_sale_no = abs(row["stock_no"])
+    sale_value = row["trx_value_pln"] #defined only for convinience
+    purchase_cost = 0.0
+    original_name = row["stock_name"]
+    normalized_name1 = original_name.replace('"','')
+    normalized_name2 = normalized_name1.replace("'","")
+    table_name = u"\u0060" + normalized_name2 + u"\u0060"
+    s = connection.cursor()
+    sql = "SELECT * from %s WHERE available_stock_no > 0 ORDER BY ID"
+    s.execute(sql % table_name)
+    result_sales = s.fetchall()
+    #Here we start iterating through the given stock table
+    #We update values in the stock table to maintain a valid state, row after row
+    #In order to be able to compare sale value vs purchse costs
+
+    for row_s in result_sales:
+
+        print 'Starting sale "stock_no" of ' + normalized_name2 + ": " + str(row_s["available_stock_no"])
+        #End of the operation - all happens within a single row
+        if row_s["available_stock_no"] >= stock_to_sale_no:
+            print "Now finishing sale of %s, using row ID: %s" % (normalized_name2, row_s["ID"])
+            purchase_cost = purchase_cost + stock_to_sale_no * row_s["purchase_price_pln"]
+
+            #update DB
+            update_stock_inventory(row_s, table_name, (row_s["available_stock_no"] - stock_to_sale_no))
+            update_sales_table(row, normalized_name2, sale_value,purchase_cost)
+            stock_to_sale_no = 0
+            break #if stock_to_sale_no is 0, we're done with this operation
+
+        #Complex path - if the sale is greater than a number of stock in the first visible purchase of that stock, we must sell all that stock, then move onto another row and keep selling
+        elif row_s["available_stock_no"] < stock_to_sale_no:
+            purchase_cost = purchase_cost + row_s["available_stock_no"] * row_s["purchase_price_pln"]
+            stock_to_sale_no = stock_to_sale_no - row_s["available_stock_no"]
+            print "Processing the sale using row ID: %s" % row_s["ID"]
+            print "Updated stock_to_sale_no " + str(stock_to_sale_no)
+            print "Updated purchase_cost " + str(purchase_cost)
+            all_sold = 0 #used only because we can't pass a 0 as a function parameter!
+            #Update DB
+            update_stock_inventory(row_s, table_name, all_sold)
+        s.close()
+    return None
+
+
+
+connection = m.connect(host='localhost', user='stockprofit', db='stockprofit', port=3306, cursorclass=pymysql.cursors.DictCursor)
+k = connection.cursor()
+k.execute('SELECT * FROM input')
+result = k.fetchall()
+for row in result:
+    print "Now processing %s, stock: %s, volume: %i" %(row["date"], row["stock_name"], row["stock_no"])
+    if row["stock_no"] > 0:
+        process_purchase(row)
+    elif row["stock_no"] < 0:
+        #pass
+        process_sale(row)
+
+    k.connection.commit()
+k.close()
