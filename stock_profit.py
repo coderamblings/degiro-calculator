58ae04e0dd0e345fbfffeb7c8e0d5ab62ba8efba
jj-owner <jj-owner@domain.com>
2018-04-23 22:56:52 +0200

    Minor changes with code readability in mind.

--- stock_profit.py
+++ stock_profit.py
@@ -10,14 +10,14 @@ import pymysql.cursors
 def process_purchase(row):
     """Gets a row read from inut table, picks selected columns and inserts into proper table"""
     """Removes double and single quotes from stock names"""
-    original_name = row["stock_name"]
-    normalized_name1 = original_name.replace('"','')
-    normalized_name2 = normalized_name1.replace("'","")
+    normalized_name = row["stock_name"]
+    normalized_name = normalized_name.replace('"','')
+    normalized_name = normalized_name.replace("'","")
     #Table name which contains spaces needs to be surrounded by ticks, thus u0060
-    table_name = u"\u0060" + normalized_name2 + u"\u0060"
+    table_name = u"\u0060" + normalized_name + u"\u0060"
     print 'Performing an insert of a purchase operation into' + table_name
     #Row value which contains spaces needs to be surrounded by quotes, thus u0022
-    stock_name = u"\u0022" + normalized_name2 + u"\u0022"
+    stock_name = u"\u0022" + normalized_name + u"\u0022"
     sql = "INSERT INTO %s (date, stock_name, trx_value_pln, available_stock_no, orig_stock_no, purchase_price_pln) VALUES (\"%s\",%s,%s,%s,%s,%s)"
     k.execute(sql % (table_name,row["date"],stock_name,row["trx_value_pln"],row["stock_no"],row["stock_no"],abs(row["trx_value_pln"]/row["stock_no"])))
     return None
@@ -45,10 +45,10 @@ def process_sale(row):
     stock_to_sale_no = abs(row["stock_no"])
     sale_value = row["trx_value_pln"] #defined only for convinience
     purchase_cost = 0.0
-    original_name = row["stock_name"]
-    normalized_name1 = original_name.replace('"','')
-    normalized_name2 = normalized_name1.replace("'","")
-    table_name = u"\u0060" + normalized_name2 + u"\u0060"
+    normalized_name = row["stock_name"]
+    normalized_name = normalized_name.replace('"','')
+    normalized_name = normalized_name.replace("'","")
+    table_name = u"\u0060" + normalized_name + u"\u0060"
     s = connection.cursor()
     sql = "SELECT * from %s WHERE available_stock_no > 0 ORDER BY ID"
     s.execute(sql % table_name)
@@ -58,20 +58,19 @@ def process_sale(row):
     #In order to be able to compare sale value vs purchse costs
 
     for row_s in result_sales:
-
-        print 'Starting sale "stock_no" of ' + normalized_name2 + ": " + str(row_s["available_stock_no"])
-        #End of the operation - all happens within a single row
+        print 'Starting sale of ' + normalized_name + ", there's " + str(row_s["available_stock_no"]) + " available in the first stock table, row ID " + str(row_s["ID"])
+        #If there's less stock to be sold than there are available stock in the first row of the given stock database, we just subtract one from the other and that's it
         if row_s["available_stock_no"] >= stock_to_sale_no:
-            print "Now finishing sale of %s, using row ID: %s" % (normalized_name2, row_s["ID"])
+            print "Now finishing sale of %s, using row ID: %s" % (normalized_name, row_s["ID"])
             purchase_cost = purchase_cost + stock_to_sale_no * row_s["purchase_price_pln"]
 
             #update DB
             update_stock_inventory(row_s, table_name, (row_s["available_stock_no"] - stock_to_sale_no))
-            update_sales_table(row, normalized_name2, sale_value,purchase_cost)
+            update_sales_table(row, normalized_name, sale_value,purchase_cost)
             stock_to_sale_no = 0
             break #if stock_to_sale_no is 0, we're done with this operation
 
-        #Complex path - if the sale is greater than a number of stock in the first visible purchase of that stock, we must sell all that stock, then move onto another row and keep selling
+        #If the sale is greater than the number of stock in the first registered purchase of that stock, we must sell all that stock, then move onto another row and keep selling
         elif row_s["available_stock_no"] < stock_to_sale_no:
             purchase_cost = purchase_cost + row_s["available_stock_no"] * row_s["purchase_price_pln"]
             stock_to_sale_no = stock_to_sale_no - row_s["available_stock_no"]
@@ -90,13 +89,13 @@ connection = m.connect(host='localhost', user='stockprofit', db='stockprofit', p
 k = connection.cursor()
 k.execute('SELECT * FROM input')
 result = k.fetchall()
+if len(result) == 0:
+    print "Couldn't find any rows in the 'input' table, double check the DB."
 for row in result:
     print "Now processing %s, stock: %s, volume: %i" %(row["date"], row["stock_name"], row["stock_no"])
     if row["stock_no"] > 0:
         process_purchase(row)
     elif row["stock_no"] < 0:
-        #pass
         process_sale(row)
-
     k.connection.commit()
 k.close()
